e4151514c4dd91bbbe240ba741aafe7b15bf2556
deniardiansyah <105403702+deniwualandari@users.noreply.github.com>
2022-05-17 11:16:55 +0700

    Add files via upload

---
+++ form.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            background-color:;
+        }
+    </style>
+</head>
+<body >
+<center>
+    <form >
+        <table>
+        Text:<input type="text">
+        <br><br>
+        Password:<input type="password">   
+        <br><br>
+        Radio 1:<input type="radio"> 
+        Radio 2:<input type="radio"> 
+        <br><br>
+        Checkbox 1:<input type="checkbox">
+        Checkbox 2:<input type="checkbox">
+        </table>
+    
+    <br>
+    Combo Box:<select>
+             <option> opsi 1 </option>
+            <option> opsi 2 </option>
+            <option> opsi 3 </option>
+        </select>
+        <br><br>
+        Textarea: <textarea name="" id="" cols="10" rows="2"></textarea>
+        <br><br>
+        <button type="simpan">Simpan</button>
+        </form>
+        
+        <br> 
+<table border="0" >
+    
+        <tr>
+            
+            <td>Text :</td>
+            <td ><input type="text"></td>
+            
+        </tr>
+        <tr>
+            <td>Password :</td>
+            <td><input type="password"> </td>
+            
+        </tr>
+    
+        <tr>
+            
+            <td><center> Laki-laki: <input type="radio" name="jk"></center>  </td>
+            <td> <center> Perempuan: <input type="radio" name="jk"> </center> </td>
+            
+        </tr>
+        <tr>
+            <td><center> Daftar :<input type="checkbox" name="hb"> </center> </td>
+            <td> <center> Login :<input type="checkbox" name="hb"> </center></td>
+            
+        </tr>
+        <tr>
+            <td> Combo Box :</td>
+            <td> <select>
+             <option> pedagang </option>
+            <option> perwira </option>
+            <option> pns </option>
+        </select> 
+    </td>
+            
+        </tr>
+        <tr>
+            <td>Textarea :</td>
+            <td> 
+            <textarea name="" id="" cols="20" rows="3"></textarea>
+            </td>
+            
+        </tr>
+        <tr>
+            
+            <td rowspan="2"  colspan="2"> <button type="simpan">Simpan</button></td>
+            
+        </tr>
+   
+</table>
+</center>
+</body>
+</html>
